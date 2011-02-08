87af02ad7ff3b8a1bac9d47313d1908890cf63bb
Michal Orman <michal.orman@gmail.com>
2011-02-08 11:26:56 +0100

    Added test for serializing reference type.

--- kvmapper-core/src/test/java/pl/michalorman/kvmapper/mapper/KVMapperTest.java
+++ kvmapper-core/src/test/java/pl/michalorman/kvmapper/mapper/KVMapperTest.java
@@ -22,14 +22,14 @@ public class KVMapperTest {
      */
 
     @DataProvider
-    public Object[][] typeValues() {
+    public Object[][] dataForTestOnPrimitives() {
         return new Object[][]{
                 { Short.MAX_VALUE, Integer.MAX_VALUE, Long.MAX_VALUE, Float.MAX_VALUE, Double.MAX_VALUE, true, Character.MAX_VALUE },
                 { Short.MIN_VALUE, Integer.MIN_VALUE, Long.MIN_VALUE, Float.MIN_VALUE, Double.MIN_VALUE, false, Character.MIN_VALUE }
         };
     }
 
-    @Test(description = "Should serializer primitive properties of an object and write to appendable.", dataProvider = "typeValues")
+    @Test(description = "Should serialize primitive properties of an object and write result to an appendable.", dataProvider = "dataForTestOnPrimitives")
     public void shouldWriteObjectToAppendable(short typeShort, int typeInt, long typeLong, float typeFloat, double typeDouble,
                                               boolean typeBoolean, char typeChar) throws IOException {
         Primitives target = new Primitives(typeShort, typeInt, typeLong, typeFloat, typeDouble, typeBoolean, typeChar);
@@ -43,6 +43,31 @@ public class KVMapperTest {
         assertTrue(result.contains("typeDouble=" + String.valueOf(typeDouble) + "\n"), "Should contain key-value pair for a typeDouble property");
         assertTrue(result.contains("typeBoolean=" + String.valueOf(typeBoolean) + "\n"), "Should contain key-value pair for a typeBoolean property");
         assertTrue(result.contains("typeChar=" + String.valueOf(typeChar) + "\n"), "Should contain key-value pair for a typeChar property");
+    }
+
+    @DataProvider
+    public Object[][] dataForTestOnTypes() {
+        return new Object[][]{
+                { Short.MAX_VALUE, Integer.MAX_VALUE, Long.MAX_VALUE, Float.MAX_VALUE, Double.MAX_VALUE, true, Character.MAX_VALUE, "Test" },
+                { Short.MIN_VALUE, Integer.MIN_VALUE, Long.MIN_VALUE, Float.MIN_VALUE, Double.MIN_VALUE, false, Character.MIN_VALUE, "Test" }
+        };
+    }
+
+    @Test(description = "Should serialize reference type properties of an object and write result to an appendable.", dataProvider = "dataForTestOnTypes")
+    public void shouldWriteObjectToAppendable(Short typeShort, Integer typeInt, Long typeLong, Float typeFloat, Double typeDouble,
+                                              Boolean typeBoolean, Character typeChar, String typeString) throws IOException {
+        Types target = new Types(typeShort, typeInt, typeLong, typeFloat, typeDouble, typeBoolean, typeChar, typeString);
+        StringBuilder builder = new StringBuilder();
+        mapper.writeObject(builder, target);
+        String result = builder.toString();
+        assertTrue(result.contains("typeShort=" + String.valueOf(typeShort) + "\n"), "Should contain key-value pair for a typeShort property");
+        assertTrue(result.contains("typeInt=" + String.valueOf(typeInt) + "\n"), "Should contain key-value pair for a typeInt property");
+        assertTrue(result.contains("typeLong=" + String.valueOf(typeLong) + "\n"), "Should contain key-value pair for a typeLong property");
+        assertTrue(result.contains("typeFloat=" + String.valueOf(typeFloat) + "\n"), "Should contain key-value pair for a typeFloat property");
+        assertTrue(result.contains("typeDouble=" + String.valueOf(typeDouble) + "\n"), "Should contain key-value pair for a typeDouble property");
+        assertTrue(result.contains("typeBoolean=" + String.valueOf(typeBoolean) + "\n"), "Should contain key-value pair for a typeBoolean property");
+        assertTrue(result.contains("typeChar=" + String.valueOf(typeChar) + "\n"), "Should contain key-value pair for a typeChar property");
+        assertTrue(result.contains("typeString=" + typeString + "\n"), "Should contain key-value pair for a typeString property");
     }
 
     public class Primitives {
@@ -121,6 +146,96 @@ public class KVMapperTest {
 
         public void setTypeChar(char typeChar) {
             this.typeChar = typeChar;
+        }
+    }
+
+    public class Types {
+        private Short typeShort;
+        private Integer typeInt;
+        private Long typeLong;
+        private Float typeFloat;
+        private Double typeDouble;
+        private Boolean typeBoolean;
+        private Character typeChar;
+        private String typeString;
+
+        public Types() {
+        }
+
+        public Types(Short typeShort, Integer typeInt, Long typeLong, Float typeFloat, Double typeDouble,
+                     Boolean typeBoolean, Character typeChar, String typeString) {
+            this.typeShort = typeShort;
+            this.typeInt = typeInt;
+            this.typeLong = typeLong;
+            this.typeFloat = typeFloat;
+            this.typeDouble = typeDouble;
+            this.typeBoolean = typeBoolean;
+            this.typeChar = typeChar;
+            this.typeString = typeString;
+        }
+
+        public Short getTypeShort() {
+            return typeShort;
+        }
+
+        public void setTypeShort(Short typeShort) {
+            this.typeShort = typeShort;
+        }
+
+        public Integer getTypeInt() {
+            return typeInt;
+        }
+
+        public void setTypeInt(Integer typeInt) {
+            this.typeInt = typeInt;
+        }
+
+        public Long getTypeLong() {
+            return typeLong;
+        }
+
+        public void setTypeLong(Long typeLong) {
+            this.typeLong = typeLong;
+        }
+
+        public Float getTypeFloat() {
+            return typeFloat;
+        }
+
+        public void setTypeFloat(Float typeFloat) {
+            this.typeFloat = typeFloat;
+        }
+
+        public Double getTypeDouble() {
+            return typeDouble;
+        }
+
+        public void setTypeDouble(Double typeDouble) {
+            this.typeDouble = typeDouble;
+        }
+
+        public Boolean getTypeBoolean() {
+            return typeBoolean;
+        }
+
+        public void setTypeBoolean(Boolean typeBoolean) {
+            this.typeBoolean = typeBoolean;
+        }
+
+        public Character getTypeChar() {
+            return typeChar;
+        }
+
+        public void setTypeChar(Character typeChar) {
+            this.typeChar = typeChar;
+        }
+
+        public String getTypeString() {
+            return typeString;
+        }
+
+        public void setTypeString(String typeString) {
+            this.typeString = typeString;
         }
     }
 }
